feb8e4c12c2d9d9b8cc39ece6b4e4cfdc6576b17
junaid1506 <junnu2757@gmail.com>
2025-12-23 10:16:49 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World !!")
+const fs = require('fs')
+fs.writeFile("Output.txt","Junaid Ansari" , (err)=>{
+  if(err){
+    console.log(err)
+  }else{
+    console.log("File Written Succesfully")
+  }
+})

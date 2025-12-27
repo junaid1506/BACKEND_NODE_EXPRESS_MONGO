cddb595c76852396d0d79450381712a1d34b3f88
junaid1506 <junnu2757@gmail.com>
2025-12-27 16:54:40 +0530

    Update

---
+++ PracticeSet/Calculator/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const routing = require("./routing");
+const PORT = 3000;
+
+const server = http.createServer(routing);
+
+server.listen(PORT, () => {
+  console.log("Server is running properly");
+});
+
+

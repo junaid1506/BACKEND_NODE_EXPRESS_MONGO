354fb3c0689918e2af25681b7a8a48bec8ff8f95
junaid1506 <junnu2757@gmail.com>
2025-12-31 13:49:59 +0530

    Update

--- 06.Introduction-of- ExpressJS/app.js
+++ 06.Introduction-of- ExpressJS/app.js
@@ -5,13 +5,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("came in first middleware" + req.url, req.method);
+  //res.send(`<h1>First Middelware`);
   next();
 });
-app.use((req, res, next) => {
+app.use("/submit", (req, res, next) => {
   console.log("came in second middleware" + req.url, req.method);
   res.send(`<h1>Welcome to JND Tech`);
+});
+app.use("/", (req, res, next) => {
+  console.log("came in another middleware" + req.url, req.method);
+  res.send(`<h1>another Middelware`);
+  next();
 });
 
 const PORT = 3001;

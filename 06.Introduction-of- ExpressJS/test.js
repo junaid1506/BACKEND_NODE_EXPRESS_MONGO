ebe15f8b266007c2e4fe9ff531c7c6c5395101db
junaid1506 <junnu2757@gmail.com>
2025-12-31 15:28:45 +0530

    Update

--- 06.Introduction-of- ExpressJS/test.js
+++ 06.Introduction-of- ExpressJS/test.js
@@ -5,9 +5,9 @@ app.use("/", (req, res, next) => {
   res.send(`<h1>Hello</h1>`);
   next();
 });
-app.get("/", (req, res, next) => {
-  res.send(`<h1>New</h1>`);
-});
+// app.get("/", (req, res, next) => {
+//   res.send(`<h1>New</h1>`);
+// });
 
 app.listen(3000, () => {
   console.log("app is running ");

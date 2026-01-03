926a50d4c231c27372b19566dab6cddba98961be
junaid1506 <junnu2757@gmail.com>
2026-01-03 15:25:39 +0530

    Update

---
+++ 09.airbnb-styling/routes/userRouter.js
@@ -0,0 +1,14 @@
+// Core module
+const path = require("path");
+
+// external Modules
+const express = require("express");
+const userRouter = express.Router();
+
+const rootDir = require("../utils/pathUtils");
+
+userRouter.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "home.html"));
+});
+
+module.exports = userRouter;

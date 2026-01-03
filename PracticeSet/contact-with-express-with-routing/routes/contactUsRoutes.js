1013d5c0f7301ae883d9bb7328ba0c5724a781b2
junaid1506 <junnu2757@gmail.com>
2026-01-03 14:42:41 +0530

    Update

--- PracticeSet/contact-with-express-with-routing/routes/contactUsRoutes.js
+++ PracticeSet/contact-with-express-with-routing/routes/contactUsRoutes.js
@@ -2,15 +2,15 @@ const express = require("express");
 // const path = require("path");
 const path = require("path");
 const pathDir = require("../utils/pathUtils");
-const userRouter = express.Router();
+const contactUsRoutes = express.Router();
 
-userRouter.get("/contact-us", (req, res, next) => {
+contactUsRoutes.get("/contact-us", (req, res, next) => {
   res.sendFile(path.join(pathDir, "views", "contact.html"));
 });
 
-userRouter.post("/contact-us", (req, res, next) => {
+contactUsRoutes.post("/contact-us", (req, res, next) => {
   console.log(req.body);
   res.sendFile(path.join(pathDir, "views", "successMsg.html"));
 });
 
-module.exports = userRouter;
+module.exports = contactUsRoutes;
--- PracticeSet/contact-with-express-with-routing/routes/homeRoutes.js
+++ PracticeSet/contact-with-express-with-routing/routes/homeRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 // const path = require("path");
 const path = require("path");
 const pathDir = require("../utils/pathUtils");
-const userRouter = express.Router();
+const homeRoutes = express.Router();
 
-userRouter.get("/", (req, res, next) => {
+homeRoutes.get("/", (req, res, next) => {
   res.sendFile(path.join(pathDir, "views", "home.html"));
 });
 
 
-module.exports = userRouter;
+module.exports = homeRoutes;

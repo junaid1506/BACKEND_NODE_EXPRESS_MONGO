da1ac2715bb196d2235c815ed166a60a2f4e6d04
junaid1506 <junnu2757@gmail.com>
2026-01-07 17:06:39 +0530

    Update

--- 11.MVC/controller/home.js
+++ 11.MVC/controller/home.js
@@ -1,20 +1,17 @@
+const Home = require("../models/home");
 
 exports.getAddHome = (req, res, next) => {
   res.render("addHome", { pageTitle: "Register Your Home" });
 };
 
 exports.postAddHome = (req, res, next) => {
-  registerHomes.push({
-    houseName: req.body.houseName,
-    localion: req.body.location,
-    price: req.body.price,
-  });
-  // console.log(registerHomes);
-
+  const home = new Home(req.body.houseName, req.body.localion, req.body.price);
+  home.save();
   res.render("successMsg", { pageTitle: "Home Registered Successfully" });
 };
 
 exports.getHome = (req, res, next) => {
+  const registerHomes = Home.fetchAll();
   console.log(registerHomes);
   res.render("home", {
     registerHomes: registerHomes,
--- 11.MVC/models/home.js
+++ 11.MVC/models/home.js
@@ -1,3 +1,8 @@
+//core module
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../utils/pathUtils");
 // fake databse
 const registerHomes = [];
 
@@ -8,5 +13,20 @@ module.exports = class Home {
     this.price = price;
   }
 
-  
+  save() {
+    registerHomes.push(this);
+    const filePath = path.join(rootDir, "data", "home.json");
+    fs.writeFile(filePath, JSON.stringify(...registerHomes), (err) => {
+      console.log("File Write Successfull");
+      console.log(`Error is ${err}`);
+    });
+  }
+
+  static fetchAll() {
+    const filePath = path.join(rootDir, "data", "home.json");
+    fs.readFile(filePath, (err, data) => {
+      console.log("File read", err, data);
+    });
+    return registerHomes;
+  }
 };

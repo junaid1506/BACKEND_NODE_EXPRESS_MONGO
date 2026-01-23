f27b8da103d99e3cf9538bd80dddb914e0b21f4a
junaid1506 <junnu2757@gmail.com>
2026-01-23 15:25:16 +0530

    Update

--- 17.Authentication-and-Autharization/controller/authController.js
+++ 17.Authentication-and-Autharization/controller/authController.js
@@ -25,7 +25,31 @@ exports.getSignup = (req, res) => {
   });
 };
 
-exports.postSignup = (req, res) => {
-  console.log(req.body);
-  res.redirect("/login");
-};
+exports.postSignup = [
+  check("name")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("Name must be at least 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("Name must contain only letters and spaces"),
+
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email address")
+    .normalizeEmail(),
+
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one number"),
+  check("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords do not match");
+    }
+  }),
+  (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/login");
+  },
+];
